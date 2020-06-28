4b48111a072202f4a0760b5cfc03ee7adcd95642
lwx <l@w.x>
2020-06-28 22:00:39 +0800

    a

---
+++ precache-manifest.2d82ec24147633bed64fd9b5dc6768b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c79f75753aecc2a045259c72c32417",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76be50240c57f23a6a34",
+    "url": "/static/css/main.b6fd2b5d.chunk.css"
+  },
+  {
+    "revision": "0d32ec431384881afc6c",
+    "url": "/static/js/2.c927cbe6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c927cbe6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76be50240c57f23a6a34",
+    "url": "/static/js/main.ace11212.chunk.js"
+  },
+  {
+    "revision": "687c8d30cb34728e6f29",
+    "url": "/static/js/runtime-main.984d8fc9.js"
+  },
+  {
+    "revision": "a88d5a78cbaab9752003323f84ddc042",
+    "url": "/static/media/checkout-icon.a88d5a78.svg"
+  },
+  {
+    "revision": "77e26c2ec84ae9140538245e644ce168",
+    "url": "/static/media/successpage-icon.77e26c2e.svg"
+  },
+  {
+    "revision": "6d2e3da3e6ffa7adc4371c3fc309c84b",
+    "url": "/static/media/successpage-tickgreen-icon.6d2e3da3.svg"
+  }
+]);
